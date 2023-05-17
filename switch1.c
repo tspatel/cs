f131b022202a8a1eb416597d0a0eebc17d4d7c82
tspatel <132879158+tspatel@users.noreply.github.com>
2023-05-16 23:53:42 -0700

    Add files via upload

---
+++ switch1.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+main()
+{
+	int a;
+	printf("enter  the number =");
+	scanf("%d",&a);
+	switch(a)
+	{
+		case 1:
+			printf("january");
+			break;
+		case 2:
+			printf("february");
+			break;
+		case 3:
+			printf("march");
+			break;
+		case 4:
+			printf("april");
+			break;
+		case 5:
+			printf("may");
+			break;
+		case 6:
+			printf("june");
+			break;
+		case 7:
+			printf("july");
+			break;
+		case 8:	
+			printf("august");
+			break;
+	    case 9:
+	        printf("septmber");
+			break;
+		case 10:
+			printf("octomber");
+			break;
+		case 11:
+			printf("november");
+			break;
+	    case 12:
+			printf("december");
+			break;
+		default:
+		printf("please enter correct value!");
+		return 0;
+		
+				
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
